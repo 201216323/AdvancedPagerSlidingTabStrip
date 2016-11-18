72ccb8bc01badaf7def1543d629d7d83f2c0edb9
1060140613@qq.com <Github@201216323>
2016-11-18 15:12:00 +0800

    增加retrofit,

--- app/src/main/java/com/lhh/apst/advancedpagerslidingtabstrip/IconTabActivity.java
+++ app/src/main/java/com/lhh/apst/advancedpagerslidingtabstrip/IconTabActivity.java
@@ -3,31 +3,33 @@ package com.lhh.apst.advancedpagerslidingtabstrip;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.RelativeLayout;
 
 import com.lhh.apst.fragments.FirstFragment;
+import com.lhh.apst.fragments.FiveFragment;
 import com.lhh.apst.fragments.FourthFragment;
 import com.lhh.apst.fragments.SecondFragment;
+import com.lhh.apst.fragments.SixFragment;
 import com.lhh.apst.fragments.ThirdFragment;
 import com.lhh.apst.library.AdvancedPagerSlidingTabStrip;
 
 /**
  * Created by linhonghong on 2015/8/11.
  */
-public class IconTabActivity extends ActionBarActivity implements ViewPager.OnPageChangeListener{
+public class IconTabActivity extends ActionBarActivity implements ViewPager.OnPageChangeListener {
 
     public AdvancedPagerSlidingTabStrip mAPSTS;
     public APSTSViewPager mVP;
 
-    private static final int VIEW_FIRST 		= 0;
-    private static final int VIEW_SECOND	    = 1;
-    private static final int VIEW_THIRD       = 2;
-    private static final int VIEW_FOURTH    = 3;
+    private static final int VIEW_FIRST = 0;
+    private static final int VIEW_SECOND = 1;
+    private static final int VIEW_THIRD = 2;
+    private static final int VIEW_FOURTH = 3;
+
+
 
     private static final int VIEW_SIZE = 4;
 
@@ -35,6 +37,7 @@ public class IconTabActivity extends ActionBarActivity implements ViewPager.OnPa
     private SecondFragment mSecondFragment = null;
     private ThirdFragment mThirdFragment = null;
     private FourthFragment mFourthFragment = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +47,12 @@ public class IconTabActivity extends ActionBarActivity implements ViewPager.OnPa
         init();
     }
 
-    private void findViews(){
-        mAPSTS = (AdvancedPagerSlidingTabStrip)findViewById(R.id.tabs);
-        mVP = (APSTSViewPager)findViewById(R.id.vp_main);
+    private void findViews() {
+        mAPSTS = (AdvancedPagerSlidingTabStrip) findViewById(R.id.tabs);
+        mVP = (APSTSViewPager) findViewById(R.id.vp_main);
     }
 
-    private void init(){
+    private void init() {
         mVP.setOffscreenPageLimit(VIEW_SIZE);
         FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
 
@@ -59,7 +62,7 @@ public class IconTabActivity extends ActionBarActivity implements ViewPager.OnPa
         mAPSTS.setViewPager(mVP);
         mAPSTS.setOnPageChangeListener(this);
         mVP.setCurrentItem(VIEW_FIRST);
-        mAPSTS.showDot(VIEW_FIRST,"99+");
+        mAPSTS.showDot(VIEW_FIRST, "99+");
     }
 
     @Override
@@ -77,7 +80,7 @@ public class IconTabActivity extends ActionBarActivity implements ViewPager.OnPa
 
     }
 
-    public class FragmentAdapter extends FragmentStatePagerAdapter implements AdvancedPagerSlidingTabStrip.IconTabProvider{
+    public class FragmentAdapter extends FragmentStatePagerAdapter implements AdvancedPagerSlidingTabStrip.IconTabProvider {
 
         public FragmentAdapter(FragmentManager fm) {
             super(fm);
@@ -85,27 +88,28 @@ public class IconTabActivity extends ActionBarActivity implements ViewPager.OnPa
 
         @Override
         public Fragment getItem(int position) {
-            if(position >= 0 && position < VIEW_SIZE){
-                switch (position){
-                    case  VIEW_FIRST:
-                        if(null == mFirstFragment)
+            if (position >= 0 && position < VIEW_SIZE) {
+                switch (position) {
+                    case VIEW_FIRST:
+                        if (null == mFirstFragment)
                             mFirstFragment = FirstFragment.instance();
                         return mFirstFragment;
 
                     case VIEW_SECOND:
-                        if(null == mSecondFragment)
+                        if (null == mSecondFragment)
                             mSecondFragment = SecondFragment.instance();
                         return mSecondFragment;
 
                     case VIEW_THIRD:
-                        if(null == mThirdFragment)
+                        if (null == mThirdFragment)
                             mThirdFragment = ThirdFragment.instance();
                         return mThirdFragment;
 
                     case VIEW_FOURTH:
-                        if(null == mFourthFragment)
+                        if (null == mFourthFragment)
                             mFourthFragment = FourthFragment.instance();
                         return mFourthFragment;
+
                     default:
                         break;
                 }
@@ -120,16 +124,16 @@ public class IconTabActivity extends ActionBarActivity implements ViewPager.OnPa
 
         @Override
         public CharSequence getPageTitle(int position) {
-            if(position >= 0 && position < VIEW_SIZE){
-                switch (position){
-                    case  VIEW_FIRST:
-                        return  "first";
-                    case  VIEW_SECOND:
-                        return  "second";
-                    case  VIEW_THIRD:
-                        return  "third";
-                    case  VIEW_FOURTH:
-                        return  "fourth";
+            if (position >= 0 && position < VIEW_SIZE) {
+                switch (position) {
+                    case VIEW_FIRST:
+                        return "first";
+                    case VIEW_SECOND:
+                        return "second";
+                    case VIEW_THIRD:
+                        return "third";
+                    case VIEW_FOURTH:
+                        return "fourth";
                     default:
                         break;
                 }
@@ -139,16 +143,16 @@ public class IconTabActivity extends ActionBarActivity implements ViewPager.OnPa
 
         @Override
         public Integer getPageIcon(int index) {
-            if(index >= 0 && index < VIEW_SIZE){
-                switch (index){
-                    case  VIEW_FIRST:
-                        return  R.mipmap.home_main_icon_n;
+            if (index >= 0 && index < VIEW_SIZE) {
+                switch (index) {
+                    case VIEW_FIRST:
+                        return R.mipmap.home_main_icon_n;
                     case VIEW_SECOND:
-                        return  R.mipmap.home_categry_icon_n;
+                        return R.mipmap.home_categry_icon_n;
                     case VIEW_THIRD:
-                        return  R.mipmap.home_live_icon_n;
+                        return R.mipmap.home_live_icon_n;
                     case VIEW_FOURTH:
-                        return  R.mipmap.home_mine_icon_n;
+                        return R.mipmap.home_mine_icon_n;
                     default:
                         break;
                 }
@@ -158,16 +162,16 @@ public class IconTabActivity extends ActionBarActivity implements ViewPager.OnPa
 
         @Override
         public Integer getPageSelectIcon(int index) {
-            if(index >= 0 && index < VIEW_SIZE){
-                switch (index){
-                    case  VIEW_FIRST:
-                        return  R.mipmap.home_main_icon_f_n;
+            if (index >= 0 && index < VIEW_SIZE) {
+                switch (index) {
+                    case VIEW_FIRST:
+                        return R.mipmap.home_main_icon_f_n;
                     case VIEW_SECOND:
-                        return  R.mipmap.home_categry_icon_f_n;
+                        return R.mipmap.home_categry_icon_f_n;
                     case VIEW_THIRD:
-                        return  R.mipmap.home_live_icon_f_n;
+                        return R.mipmap.home_live_icon_f_n;
                     case VIEW_FOURTH:
-                        return  R.mipmap.home_mine_icon_f_n;
+                        return R.mipmap.home_mine_icon_f_n;
                     default:
                         break;
                 }
--- app/src/main/java/com/lhh/apst/advancedpagerslidingtabstrip/NormalTabActivity.java
+++ app/src/main/java/com/lhh/apst/advancedpagerslidingtabstrip/NormalTabActivity.java
@@ -1,42 +1,49 @@
 package com.lhh.apst.advancedpagerslidingtabstrip;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 
+import com.lhh.apst.fragments.EightFragment;
 import com.lhh.apst.fragments.FirstFragment;
+import com.lhh.apst.fragments.FiveFragment;
 import com.lhh.apst.fragments.FourthFragment;
 import com.lhh.apst.fragments.SecondFragment;
+import com.lhh.apst.fragments.SevenFragment;
+import com.lhh.apst.fragments.SixFragment;
 import com.lhh.apst.fragments.ThirdFragment;
 import com.lhh.apst.library.AdvancedPagerSlidingTabStrip;
 
 /**
  * Created by linhonghong on 2015/8/10.
  */
-public class NormalTabActivity extends ActionBarActivity implements ViewPager.OnPageChangeListener{
+public class NormalTabActivity extends ActionBarActivity implements ViewPager.OnPageChangeListener {
 
     public AdvancedPagerSlidingTabStrip mAPSTS;
     public APSTSViewPager mVP;
 
-    private static final int VIEW_FIRST 		= 0;
-    private static final int VIEW_SECOND	    = 1;
-    private static final int VIEW_THIRD       = 2;
-    private static final int VIEW_FOURTH    = 3;
+    private static final int VIEW_FIRST = 0;
+    private static final int VIEW_SECOND = 1;
+    private static final int VIEW_THIRD = 2;
+    private static final int VIEW_FOURTH = 3;
+    private static final int VIEW_FIVE = 4;
+    private static final int VIEW_SIX = 5;
+    private static final int VIEW_SEVEN = 6;
+    private static final int VIEW_EIGH = 7;
 
-    private static final int VIEW_SIZE = 4;
+    private static final int VIEW_SIZE = 8;
 
     private FirstFragment mFirstFragment = null;
     private SecondFragment mSecondFragment = null;
     private ThirdFragment mThirdFragment = null;
     private FourthFragment mFourthFragment = null;
+    private FiveFragment mFiveFragment = null;
+    private SixFragment mSixFragment = null;
+    private SevenFragment mSevenFragment = null;
+    private EightFragment mEightFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +53,12 @@ public class NormalTabActivity extends ActionBarActivity implements ViewPager.On
         init();
     }
 
-    private void findViews(){
-        mAPSTS = (AdvancedPagerSlidingTabStrip)findViewById(R.id.tabs);
-        mVP = (APSTSViewPager)findViewById(R.id.vp_main);
+    private void findViews() {
+        mAPSTS = (AdvancedPagerSlidingTabStrip) findViewById(R.id.tabs);
+        mVP = (APSTSViewPager) findViewById(R.id.vp_main);
     }
 
-    private void init(){
+    private void init() {
         mVP.setOffscreenPageLimit(VIEW_SIZE);
         FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
 
@@ -60,7 +67,7 @@ public class NormalTabActivity extends ActionBarActivity implements ViewPager.On
         adapter.notifyDataSetChanged();
         mAPSTS.setViewPager(mVP);
         mAPSTS.setOnPageChangeListener(this);
-        mAPSTS.showDot(VIEW_FIRST);
+//        mAPSTS.showDot(VIEW_FIRST);
     }
 
     @Override
@@ -78,7 +85,7 @@ public class NormalTabActivity extends ActionBarActivity implements ViewPager.On
 
     }
 
-    public class FragmentAdapter extends FragmentStatePagerAdapter{
+    public class FragmentAdapter extends FragmentStatePagerAdapter {
 
         public FragmentAdapter(FragmentManager fm) {
             super(fm);
@@ -86,27 +93,44 @@ public class NormalTabActivity extends ActionBarActivity implements ViewPager.On
 
         @Override
         public Fragment getItem(int position) {
-            if(position >= 0 && position < VIEW_SIZE){
-                switch (position){
-                    case  VIEW_FIRST:
-                        if(null == mFirstFragment)
+            if (position >= 0 && position < VIEW_SIZE) {
+                switch (position) {
+                    case VIEW_FIRST:
+                        if (null == mFirstFragment)
                             mFirstFragment = FirstFragment.instance();
                         return mFirstFragment;
 
                     case VIEW_SECOND:
-                        if(null == mSecondFragment)
+                        if (null == mSecondFragment)
                             mSecondFragment = SecondFragment.instance();
                         return mSecondFragment;
 
                     case VIEW_THIRD:
-                        if(null == mThirdFragment)
+                        if (null == mThirdFragment)
                             mThirdFragment = ThirdFragment.instance();
                         return mThirdFragment;
 
                     case VIEW_FOURTH:
-                        if(null == mFourthFragment)
+                        if (null == mFourthFragment)
                             mFourthFragment = FourthFragment.instance();
                         return mFourthFragment;
+                    case VIEW_FIVE:
+                        if (null == mFiveFragment)
+                            mFiveFragment = FiveFragment.instance();
+                        return mFiveFragment;
+                    case VIEW_SIX:
+                        if (null == mSixFragment)
+                            mSixFragment = SixFragment.instance();
+                        return mSixFragment;
+                    case VIEW_SEVEN:
+                        if (null == mSevenFragment)
+                            mSevenFragment = SevenFragment.instance();
+                        return mSevenFragment;
+
+                    case VIEW_EIGH:
+                        if (null == mEightFragment)
+                            mEightFragment = EightFragment.instance();
+                        return mEightFragment;
                     default:
                         break;
                 }
@@ -121,16 +145,27 @@ public class NormalTabActivity extends ActionBarActivity implements ViewPager.On
 
         @Override
         public CharSequence getPageTitle(int position) {
-            if(position >= 0 && position < VIEW_SIZE){
-                switch (position){
-                    case  VIEW_FIRST:
-                        return  "FIRST";
-                    case  VIEW_SECOND:
-                        return  "SECOND";
-                    case  VIEW_THIRD:
-                        return  "THIRD";
-                    case  VIEW_FOURTH:
-                        return  "FOURTH";
+            if (position >= 0 && position < VIEW_SIZE) {
+                switch (position) {
+                    case VIEW_FIRST:
+                        return "张无忌";
+                    case VIEW_SECOND:
+                        return "范遥";
+                    case VIEW_THIRD:
+                        return "杨逍";
+                    case VIEW_FOURTH:
+                        return "紫衫龙王";
+                    case VIEW_FIVE:
+
+                        return "白眉鹰王";
+                    case VIEW_SIX:
+                        return "金毛狮王";
+
+                    case VIEW_SEVEN:
+
+                        return "白眉鹰王";
+                    case VIEW_EIGH:
+                        return "金毛狮王";
                     default:
                         break;
                 }
